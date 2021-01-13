fa7d5019670e7e5216d9c0ae8bacfdf338ef4b3d
danielbernalb <danielbernalb@gmail.com>
2021-01-13 16:04:56 -0500

    Requested changes

--- examples/co2_cm1106/src/main.cpp
+++ examples/co2_cm1106/src/main.cpp
@@ -19,10 +19,7 @@
 #include <Sensors.hpp>
 
 void onSensorDataOk() {
-    Serial.print ("-->[MAIN] PM1: "+sensors.getStringPM1());
-    Serial.print (" PM2.5: " + sensors.getStringPM25());
-    Serial.print (" PM10: " + sensors.getStringPM10());
-    
+   
     Serial.print (" CO2: " + sensors.getStringCO2());
     Serial.print (" CO2humi: " + String(sensors.getCO2humi()));
     Serial.print (" CO2temp: " + String(sensors.getCO2temp()));
