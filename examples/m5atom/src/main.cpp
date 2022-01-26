9403ca1b397efee90df136bfb35cfcb2df4fadf6
Hpsaturn <hpsaturn@gmail.com>
2022-01-26 22:07:26 +0100

    added m5atom example and CI test

---
+++ examples/m5atom/src/main.cpp
@@ -0,0 +1,78 @@
+#include <Arduino.h>
+#include <M5Atom.h>
+#include <Sensors.hpp>
+
+void printSensorsDetected() {
+    uint16_t sensors_count =  sensors.getSensorsRegisteredCount();
+    uint16_t units_count   =  sensors.getUnitsRegisteredCount();
+    Serial.println("-->[MAIN] Sensors detected count\t: " + String(sensors_count));
+    Serial.println("-->[MAIN] Sensors units count  \t: " + String(units_count));
+    Serial.print(  "-->[MAIN] Sensors devices names\t: ");
+    int i = 0;
+    while (sensors.getSensorsRegistered()[i++] != 0) {
+        Serial.print(sensors.getSensorName((SENSORS)sensors.getSensorsRegistered()[i - 1]));
+        Serial.print(",");
+    }
+    Serial.println();
+}
+
+void printSensorsValues() {
+    Serial.println("-->[MAIN] Preview sensor values:");
+    UNIT unit = sensors.getNextUnit();
+    while(unit != UNIT::NUNIT) {
+        String uName = sensors.getUnitName(unit);
+        float uValue = sensors.getUnitValue(unit);
+        String uSymb = sensors.getUnitSymbol(unit);
+        Serial.println("-->[MAIN] " + uName + " \t: " + String(uValue) + " " + uSymb);
+        unit = sensors.getNextUnit();
+    }
+}
+
+void onSensorDataOk() {
+    Serial.println("======= E X A M P L E   T E S T =========");
+    printSensorsDetected();
+    printSensorsValues(); 
+    Serial.println("=========================================");
+}
+
+void onSensorDataError(const char * msg){ 
+}
+
+/******************************************************************************
+*  M A I N
+******************************************************************************/
+
+void setup() {
+    Serial.begin(115200);
+    delay(200);
+    Serial.println("\n== Sensor test setup ==\n");
+    M5.begin(true,true,true); //Init M5Atom(Initialization of external I2C is also included).  M5Atom(初始化外部I2C也包含在内)
+    //Wire.begin(21, 22); //Detect internal I2C, if this sentence is not added, it will detect external I2C.  检测内部I2C,若不加此句为检测外部I2C
+    Wire.begin(26,32);
+
+    Serial.println("-->[SETUP] Detecting sensors..");
+
+    sensors.setSampleTime(5);                       // config sensors sample time interval
+    sensors.setOnDataCallBack(&onSensorDataOk);     // all data read callback
+    sensors.setOnErrorCallBack(&onSensorDataError); // [optional] error callback
+    sensors.setDebugMode(true);                     // [optional] debug mode
+    sensors.detectI2COnly(true);                    // disable force to only i2c sensors
+    sensors.init();                                 // Auto detection to UART and i2c sensors
+
+    // Alternatives only for UART sensors (TX/RX):
+
+    // sensors.init(sensors.Auto);                  // Auto detection to UART sensors (Honeywell, Plantower, Panasonic)
+    // sensors.init(sensors.Panasonic);             // Force UART detection to Panasonic sensor
+    // sensors.init(sensors.Sensirion);             // Force UART detection to Sensirion sensor
+    // sensors.init(sensors.Mhz19);                 // Force UART detection to Mhz14 or Mhz19 CO2 sensor
+    // sensors.init(sensors.SDS011);                // Force UART detection to SDS011 sensor
+    // sensors.init(sensors.CM1106);                // Force UART detection to CM1106 CO2 sensor
+    // sensors.init(sensors.SENSEAIRS8);            // Force UART detection to SenseAirS8 CO2 sensor
+    // sensors.init(sensors.Auto,PMS_RX,PMS_TX);    // Auto detection on custom RX,TX
+  
+
+}
+
+void loop() {
+    sensors.loop();  // read sensor data and showed it
+}
