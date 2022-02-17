a58828a527fb263ba6a96cbebc661eb2e29681dc
Hpsaturn <hpsaturn@gmail.com>
2022-02-17 12:16:53 +0100

    testing solar mode flag from firmware preset

--- src/Sensors.hpp
+++ src/Sensors.hpp
@@ -160,6 +160,9 @@ class Sensors {
     // only detect i2c sensors
     bool i2conly;
 
+    // Solar station mode (power saving features)
+    bool solarmode = false;
+
     /*****************************************
      * I2C sensors:
      ****************************************/
