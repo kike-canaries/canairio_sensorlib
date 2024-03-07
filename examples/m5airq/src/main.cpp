ba50613994ed69aa710d4c46088fcdd8bfb8ed43
Hpsaturn <hpsaturn@gmail.com>
2024-03-07 13:28:00 -0500

    fixed SEN5X init issue (bad pin level choosed)

--- examples/m5airq/src/main.cpp
+++ examples/m5airq/src/main.cpp
@@ -17,8 +17,19 @@
 
 #include <Arduino.h>
 #include <Sensors.hpp>
+#include <I2C_BM8563.h>
 
-#define MAIN_HW_EN_PIN 10 // M5AirQ sensor hardware enable
+#define POWER_HOLD 46     // M5AirQ main board
+#define SEN55_POWER_EN 10
+
+#define GROVE_SDA 13
+#define GROVE_SCL 15
+
+#define I2C1_SDA_PIN 11
+#define I2C1_SCL_PIN 12
+
+
+I2C_BM8563 bm8563(I2C_BM8563_DEFAULT_ADDRESS, Wire);
 
 void printSensorsDetected() {
     uint16_t sensors_count =  sensors.getSensorsRegisteredCount();
@@ -60,21 +71,27 @@ void onSensorDataError(const char * msg){
 
 void powerEnableSensors() {
     Serial.println("-->[POWR] == enable sensors ==");
-    pinMode(MAIN_HW_EN_PIN, OUTPUT);
-    digitalWrite(MAIN_HW_EN_PIN, HIGH);  // step-up on
+    pinMode(POWER_HOLD, OUTPUT);
+    digitalWrite(POWER_HOLD, HIGH);
+    pinMode(SEN55_POWER_EN, OUTPUT);
+    digitalWrite(SEN55_POWER_EN, LOW);
 }
 
 void setup() {
     Serial.begin(115200);
     delay(2000);           // Only for debugging
     powerEnableSensors(); // M5AirQ enable sensors
+    Wire.begin(I2C1_SDA_PIN, I2C1_SCL_PIN);
+    Serial.println("-->[SETUP] RTC(BM8563) init");
+    bm8563.begin();
+    bm8563.clearIRQ();
     delay(100);
     Serial.println("\n== Sensor test setup ==\n");
     Serial.println("-->[SETUP] Detecting sensors..");
     
     sensors.setSampleTime(10);                       // config sensors sample time interval
     sensors.setOnDataCallBack(&onSensorDataOk);      // all data read callback
-    sensors.setDebugMode(false);                     // [optional] debug mode
+    sensors.setDebugMode(true);                     // [optional] debug mode
     sensors.detectI2COnly(false);                    // not force to only i2c sensors
     sensors.setTemperatureUnit(TEMPUNIT::CELSIUS);   // comment for Celsius or set Fahrenheit
     sensors.init();                                  // Auto detection (UART and i2c sensors)
